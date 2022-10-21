12ddab4a5be291f4c248e451e6951a70b907f695
Lalá Nametala <lalanametala@gmail.com>
2022-10-21 10:06:55 -0300

    :test_tube: test(TestFirstReq): implements the TestRegisterAccountSucess and TestRegisterAccountException methods

--- src/trybank.Test/TestFirstReq.cs
+++ src/trybank.Test/TestFirstReq.cs
@@ -8,16 +8,24 @@ namespace trybank.Test;
 public class TestFirstReq
 {
     [Theory(DisplayName = "Deve cadastrar contas com sucesso!")]
-    [InlineData(0, 0, 0)]
+    [InlineData(111, 111, 777)]
     public void TestRegisterAccountSucess(int number, int agency, int pass)
     {        
-        throw new NotImplementedException();
+        Trybank instance = new();
+        instance.RegisterAccount(number, agency, pass);
+        instance.registeredAccounts.Should().Be(1);
+        instance.Bank[0,0].Should().Be(number);
+        instance.Bank[0,1].Should().Be(agency);
+        instance.Bank[0,2].Should().Be(pass);
     }
 
     [Theory(DisplayName = "Deve retornar ArgumentException ao cadastrar contas que já existem")]
-    [InlineData(0, 0, 0)]
+    [InlineData(111, 111, 777)]
     public void TestRegisterAccountException(int number, int agency, int pass)
     {        
-        throw new NotImplementedException();
+        Trybank instance = new();
+        instance.RegisterAccount(number, agency, pass);
+        Action act = () => instance.RegisterAccount(number, agency, pass);
+        act.Should().Throw<ArgumentException>().WithMessage("A conta já está sendo usada!");
     }
 }

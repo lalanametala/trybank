6e8386a298e629e7af7eaf811012ba120af1f2b9
Lalá Nametala <lalanametala@gmail.com>
2022-10-21 20:49:53 -0300

    :sparkles: feat(Trybank): implements the Logout method

--- src/trybank/Trybank.cs
+++ src/trybank/Trybank.cs
@@ -75,7 +75,10 @@ public class Trybank
 
     public void Logout()
     {
-        throw new NotImplementedException();
+        if(!Logged) throw new AccessViolationException("Usuário não está logado");
+
+        Logged = false;
+        loggedUser = -99;
     }
 
     public int CheckBalance()
